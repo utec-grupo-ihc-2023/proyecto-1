0909736d38d2ab08c7a54f6189e8014c41faf537
otreblan <otreblain@gmail.com>
2023-09-25 19:39:50 -0500

    chore(cs): Add YOLO script

---
+++ Assets/Scripts/YOLO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Barracuda;
+using UnityEngine;
+
+public class YOLO : MonoBehaviour
+{
+    public NNModel model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
