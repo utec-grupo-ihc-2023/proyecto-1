b97b6eb7680451e343de49593e88798283eb0deb
otreblan <otreblain@gmail.com>
2023-09-25 21:40:25 -0500

    feat(YOLO): Load model and worker

--- Assets/Scripts/YOLO.cs
+++ Assets/Scripts/YOLO.cs
@@ -6,22 +6,32 @@ using UnityEngine;
 
 public class YOLO : MonoBehaviour
 {
-    public NNModel model;
+    public NNModel nnModel;
 
     public Camera inputCamera;
 
     public TMP_Text outputText;
 
+    Model model;
+    IWorker worker;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        model = ModelLoader.Load(nnModel);
+        worker = WorkerFactory.CreateWorker(model);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         // TODO: Update text with the model output
         outputText.text = "a";
+    }
+
+    void OnDestroy()
+    {
+        worker.Dispose();
     }
 }
