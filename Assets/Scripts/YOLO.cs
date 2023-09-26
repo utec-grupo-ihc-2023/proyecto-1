e773302b34cc86156623e8dfd6a8f2ca64f22bf9
otreblan <otreblain@gmail.com>
2023-09-25 21:26:05 -0500

    fix(ui): TextMeshProUGUI -> TMP_Text

--- Assets/Scripts/YOLO.cs
+++ Assets/Scripts/YOLO.cs
@@ -10,7 +10,7 @@ public class YOLO : MonoBehaviour
 
     public Camera inputCamera;
 
-    public TextMeshProUGUI outputText;
+    public TMP_Text outputText;
 
     // Start is called before the first frame update
     void Start()
