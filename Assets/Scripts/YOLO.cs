cdc284c36cfcd103e45ab75dcef301eb144465f0
otreblan <otreblain@gmail.com>
2023-09-28 11:06:53 -0500

    feat(yolo): Postprocess

--- Assets/Scripts/YOLO.cs
+++ Assets/Scripts/YOLO.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using TMPro;
 using Unity.Barracuda;
@@ -96,15 +97,63 @@ public class YOLO : MonoBehaviour
         return result;
     }
 
+    string Index2Name(int i)
+    {
+        return i switch
+        {
+            0 => "A",
+            1 => "B",
+            2 => "C",
+            3 => "D",
+            4 => "E",
+            5 => "F",
+            6 => "G",
+            7 => "I",
+            8 => "K",
+            9 => "L",
+            10 => "M",
+            11 => "N",
+            12 => "O",
+            13 => "P",
+            14 => "Q",
+            15 => "R",
+            16 => "S",
+            17 => "T",
+            18 => "U",
+            _ => "",
+        };
+    }
+
+    string Postprocess(Tensor data)
+    {
+        for(int box = 0; box < data.width; box++)
+        {
+            float confidence = data[0, 0, box, 4];
+
+            List<float> classProbabilities = new();
+            for(int probI = 5; probI < data.channels; probI++)
+            {
+                classProbabilities.Add(data[0, 0, box, probI]);
+            }
+
+            int maxI = classProbabilities.Any()? classProbabilities.IndexOf(classProbabilities.Max()) : 0;
+
+            if(confidence >= 0.1)
+            {
+                return Index2Name(maxI);
+            }
+        }
+
+        return "";
+    }
+
     string Run(Texture2D texture2D)
     {
         Tensor input = new(ResizeTexture(texture2D, 640, 640));
 
         Tensor output = worker.Execute(input).PeekOutput();
 
-        // TODO: Postprocess output
-
-        string res = output.shape.ToString();
+        string res = Postprocess(output);
 
         input.Dispose();
         output.Dispose();
